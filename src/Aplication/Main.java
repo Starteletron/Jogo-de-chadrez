55241063776ae59a2296c9c97d00ed2afac94d87
Starteletron <diascharles53@gmail.com>
2023-07-20 21:02:31 -0300

    implementacao do Board e Piece

--- src/Aplication/Main.java
+++ src/Aplication/Main.java
@@ -1,10 +1,18 @@
 package Aplication;
 
+<<<<<<< HEAD
+=======
+import boardgame.Board;
+>>>>>>> 2d01795 (implementacao do Board e Piece)
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         Position pos = new Position(3,5);
         System.out.print(pos);
+=======
+        Board board = new Board(8,8);
+>>>>>>> 2d01795 (implementacao do Board e Piece)
     }
 }
